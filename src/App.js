058839e98e73af00e03f154368a3856ae12698c7
Wingo <wingowingo3210@gmail.com>
2022-03-17 00:17:26 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import FetchFutureWallet from "./components/api/get/FetchFutureWallet";
+import FetchFutureWallet from "./components/api/get/FetchFutureWallet/FetchFutureWallet";
 import FetchFuturesPosition from "./components/api/get/FetchFuturePosition";
 import "./App.scss";
 function App() {
